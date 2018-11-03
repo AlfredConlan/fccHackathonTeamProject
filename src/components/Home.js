3d66912bbbd55f2b158c9defb2e17ebdb133add6
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-11-03 11:22:59 -0500

    styled home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react"
+import uploadImage from "../images/image_upload.png"
 
 const Home = () => (
   <header>
     <div className="container">
       <div className="main-content">
         <div>
-          <img src="" alt="" />
+          <img src={uploadImage} alt="upload image" />
+          <p>Upload an image to detect any disease</p>
           <button>Upload Image</button>
         </div>
       </div>
