81db0d2196e2f4f531d7921f57dffd1053f52f23
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-11-03 11:00:27 -0500

    created Home component

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Home = () => (
+  <header>
+    <div className="container">
+      <div className="main-content">
+        <div>
+          <img src="" alt="" />
+          <button>Upload Image</button>
+        </div>
+      </div>
+    </div>
+  </header>
+)
+
+export default Home
