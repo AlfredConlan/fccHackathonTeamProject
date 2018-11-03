8e5eba6e03c1aff3329e9b03dec3cbfc3f945267
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-11-03 13:28:39 -0500

    created Info component

---
+++ src/components/Info.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Info extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the info component.</p>
+      </div>
+    )
+  }
+}
+
+export default Info
