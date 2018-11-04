7c2304d46cff8ce78efef256b2287ddd5eca2907
AlfredConlan <oconellan@gmail.com>
2018-11-04 02:42:03 +0000

    error resolving firebase in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,25 @@
 import React from "react"
 import uploadImage from "../images/image_upload.png"
+import FileUploader from "react-firebase-file-uploader"
+import firebase from 'firebase';
 
 const Home = () => (
   <header>
     <div className="container">
       <div className="main-content">
         <div>
-          <img src={uploadImage} alt="upload image" />
+          <img src={uploadImage} alt="upload" />
           <p>Upload an image to detect any disease</p>
           <button>Upload Image</button>
+          <FileUploader
+            hidden
+            accept="image/*"
+            storageRef={firebase.storage().ref('images')}
+            onUploadStart={this.handleUploadStart}
+            onUploadError={this.handleUploadError}
+            onUploadSuccess={this.handleUploadSuccess}
+            onProgress={this.handleProgress}
+          />
         </div>
       </div>
     </div>
