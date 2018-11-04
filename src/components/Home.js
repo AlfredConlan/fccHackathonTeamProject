73e4ffdae2ab9d27684116780384a3e29bf5c0a3
AlfredConlan <oconellan@gmail.com>
2018-11-04 19:43:00 +0000

    added firestore functionality to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,84 @@
-import React from "react"
-import uploadImage from "../images/image_upload.png"
+import React, { Component } from 'react';
+import { firebaseAuth } from '../config/constants';
+import FileUploader from 'react-firebase-file-uploader';
+import firebase from 'firebase';
+import uploadImage from '../images/image_upload.png';
 
-const Home = () => (
-  <header>
-    <div className="container">
-      <div className="main-content">
-        <div>
-          <img src={uploadImage} alt="upload" />
-          <p>Upload an image to detect a disease</p>
-          <button>Upload Image</button>
+const firebaseAuthKey = 'firebaseAuthInProgress';
+const appTokenKey = 'appToken';
+
+export default class Home extends Component {
+
+  async handleUploadSuccess(filename) {
+    try {
+      let { bucket, fullPath } = await firebase.storage().ref('images').child(filename).getMetadata();
+      console.log('bucket', bucket)
+      console.log('fullPath', fullPath)
+      let downloadURL = await firebase.storage().ref('images').child(filename).getDownloadURL();
+      console.log('downloadURL', downloadURL)
+      let { uid, email, displayName } = await firebase.auth().currentUser;
+      let newPhoto = {
+        url: downloadURL,
+        userName: displayName,
+        userId: uid,
+        email,
+        bucket,
+        fullPath
+      }
+      console.log('newPhoto', newPhoto);
+      await firebase.firestore().collection('photos').add(newPhoto);
+    }
+    catch (err) {
+      console.error(err);
+    }
+  }
+
+  render() {
+    return (
+      <header>
+        <div className="container">
+          <div className="main-content">
+            <div>
+              <img src={uploadImage} alt="upload" />
+              <p>Upload an image to detect a disease</p>
+              <button onClick={() => console.log("button was pressed")}>Upload Image</button>              
+              <FileUploader
+                hidden
+                accept="image/*"
+                storageRef={firebase.storage().ref('images')}
+                onUploadStart={this.handleUploadStart}
+                onUploadError={this.handleUploadError}
+                onUploadSuccess={this.handleUploadSuccess}
+                onProgress={this.handleProgress}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </header>
-)
+      </header>
+    )
+  }
+}
 
-export default Home
+firebaseAuth().onAuthStateChanged(user => {
+  if (user) {
+    localStorage.removeItem(firebaseAuthKey);
+    localStorage.setItem(appTokenKey, user.uid);
+    this.props.history.push('/app/home')
+  }
+})
+
+// const Home = () => (
+//   <header>
+//     <div className="container">
+//       <div className="main-content">
+//         <div>
+//           <img src={uploadImage} alt="upload" />
+//           <p>Upload an image to detect a disease</p>
+//           <button>Upload Image</button>
+//         </div>
+//       </div>
+//     </div>
+//   </header>
+// )
+
+// export default Home
